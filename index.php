9450931465972ceb025231b32f7a85cdb13a42ee
Joliejo <45091405+Joliejo@users.noreply.github.com>
2018-12-12 13:58:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<body>
+<div class="wrap">
+	<nav>
+		
+		<nav>
+		
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="front-end/login.php">log in</a></li>
+			<li><a href="front-end/signup.php">sign up</a></li>
+			<li><a href="front-end/contact.php">contact us</a></li>
+			<li><a href="front-end/about.php">about</a></li>
+		</ul>
+	</nav>
+	</nav>
+
+	<div class="maintext">
+			<p>A promise of premium</p>
+	<h1> stay experience like a royalty</h1>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+<div class="btn-cont">
+		<a href="front-end/login.php" class="button">login</a>
+	<a href="front-end/signup.php" class="button">signup</a>
+</div>
+
+
+
+
+
+
+	</div>
+
+</div>
+</body>
+</html>
